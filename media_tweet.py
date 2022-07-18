889c1d69a8fdc99a540ed00fe8537d8b995aeb93
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 22:53:15 +0200

    update: 🚀refactorizacion de la funcion

--- media_tweet.py
+++ media_tweet.py
@@ -1,29 +1,20 @@
 import tweepy
 from api_data import *
+from autentificacion import autentificar
 
-def upload_media(api_key, api_secret, access_token, access_secret, img_path, text=""):
+def upload_media(api, img_path, text=""):
     
     '''
     Esta funcion manda una imagen y opcionalmente un texto
 
-    api_key: Token generado por twitter
-    api_secret: Token generado por twitter
-    access_token: Token generado por twitter
-    access_secret: Token generado por twitter
+    api = Objeto devuelto por la funcion <autentificar>
     img_path: Ruta a la imagen, video o sonido que queremos twittear
     text: (Opcional) Mensaje a twittear
     '''
-    try:
-        auth = tweepy.OAuthHandler(api_key,api_secret)
-        auth.set_access_token(access_token,access_secret)
-        api = tweepy.API(auth)
-        api.verify_credentials()
-        print('Autentificacion exitosa')
-    except:
-        print('Autentificacion de credencialles fallida')
     media = api.media_upload(img_path)
     api.update_status(text, media_ids=[media.media_id_string])
     print("Tweet enviado")      
 
 if __name__ == "__main__":
-    upload_media( api_key, api_secret, access_token, access_secret, "imgs/img_0.png", "TEST")
+    api = autentificar(api_key, api_secret, access_token, access_secret)
+    upload_media(api, "imgs/img_0.png", "TEST")
