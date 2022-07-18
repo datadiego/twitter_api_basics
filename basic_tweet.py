74daa5472fdc0cf551ce6b6b1711966d3e6eab50
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 16:28:20 +0200

    feat: 👍añadida funcion de tweet básica

---
+++ basic_tweet.py
@@ -0,0 +1,31 @@
+import tweepy, datetime
+from api_data import *
+
+def create_tweet(text, api_key, api_secret, access_token, access_secret):
+    '''
+    Esta funcion manda un tweet con el texto introducido
+
+    Parámetros
+    text: Mensaje a twittear
+    api_key: Token generado por twitter
+    api_secret: Token generado por twitter
+    access_token: Token generado por twitter
+    access_secret: Token generado por twitter
+    '''
+    try:
+        #Autentificar con la api de twitter:
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_secret)
+        api = tweepy.API(auth)
+        api.verify_credentials()
+        print('Successful Authentication')
+
+        #Mandar tweet
+        api.update_status(text)
+        ahora = datetime.datetime.now()
+        print("Tweet enviado a las", ahora)
+    except:
+        print('Failed authentication')
+
+if __name__ == "__main__":
+    create_tweet("TEST", api_key, api_secret, access_token, access_secret)
