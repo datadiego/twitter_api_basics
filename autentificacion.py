eeda95c5abac06a7efc828e93bdd8064d05acb80
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 20:57:54 +0200

    feat: 👍funcion de autentificar creada

---
+++ autentificacion.py
@@ -0,0 +1,16 @@
+from api_data import *
+import tweepy
+
+def autentificar(api_key, api_secret, access_token, access_secret):
+    try:
+        #Autentificar con la api de twitter:
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_secret)
+        api = tweepy.API(auth)
+        api.verify_credentials()
+        print('Autentificación exitosa')       
+    except:
+        print('Autentificación fallida')
+
+if __name__ == "__main__":
+    autentificar(api_key, api_secret, access_token, access_secret)
