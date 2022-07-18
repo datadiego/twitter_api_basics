8fefbd7092fcd46f906450dc14feab6eba2c26a6
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 23:01:38 +0200

    update: 🚀refacctorizacion argumentos de entrada de funcion

--- random_media_tweet.py
+++ random_media_tweet.py
@@ -4,27 +4,17 @@ import os
 import tweepy
 
 from api_data import *
+from autentificacion import autentificar
 
-def upload_random_media(api_key, api_secret, access_token, access_secret, img_path, text=""):
+def upload_random_media(api, img_path, text=""):
     
     '''
     Esta funcion manda una imagen y opcionalmente un texto
 
-    api_key: Token generado por twitter
-    api_secret: Token generado por twitter
-    access_token: Token generado por twitter
-    access_secret: Token generado por twitter
+    api: Objeto devuelto por la funcion <autentificar>
     img_path: Ruta a la imagen, video o sonido que queremos twittear
     text: (Opcional) Mensaje a twittear
     '''
-    try:
-        auth = tweepy.OAuthHandler(api_key,api_secret)
-        auth.set_access_token(access_token,access_secret)
-        api = tweepy.API(auth)
-        api.verify_credentials()
-        print('Autentificacion exitosa')
-    except:
-        print('Autentificacion de credencialles fallida')
     media = api.media_upload(img_path)
     api.update_status(text, media_ids=[media.media_id_string])
     print("Tweet enviado")      
@@ -35,4 +25,5 @@ if __name__ == "__main__":
     target_img = choice(files)
     img_path += target_img
     print(img_path)
-    upload_random_media(api_key, api_secret, access_token, access_secret, img_path)
+    api = autentificar(api_key, api_secret, access_token, access_secret)
+    upload_random_media(api, img_path)
