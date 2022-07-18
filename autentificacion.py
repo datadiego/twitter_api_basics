2398e79de3c9ece303858a650c240d38c10fc3f4
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 22:38:57 +0200

    update: 🧰añadidos comentarios y parametros de la funcion

--- autentificacion.py
+++ autentificacion.py
@@ -2,6 +2,15 @@ from api_data import *
 import tweepy
 
 def autentificar(api_key, api_secret, access_token, access_secret):
+    """
+    Esta funcion verifica tus credenciales en la API de twitter.
+
+    Parámetros:
+    api_key: Token proporcionado por twitter
+    api_secret: Token proporcionado por twitter
+    access_token: Token proporcionado por twitter
+    access_secret: Token propocionado por twitter
+    """
     try:
         #Autentificar con la api de twitter:
         auth = tweepy.OAuthHandler(api_key, api_secret)
