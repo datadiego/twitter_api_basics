fb3bd15111768ddd2968016c76d1f86d0be9c518
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 16:43:27 +0200

    update: 🧰refactorizacion de la funcion

--- basic_tweet.py
+++ basic_tweet.py
@@ -18,14 +18,14 @@ def create_tweet(text, api_key, api_secret, access_token, access_secret):
         auth.set_access_token(access_token, access_secret)
         api = tweepy.API(auth)
         api.verify_credentials()
-        print('Successful Authentication')
-
-        #Mandar tweet
-        api.update_status(text)
-        ahora = datetime.datetime.now()
-        print("Tweet enviado a las", ahora)
+        print('Autentificación exitosa')       
     except:
-        print('Failed authentication')
+        print('Autentificación fallida')
+    
+    #Mandar tweet
+    api.update_status(text)
+    ahora = datetime.datetime.now()
+    print("Tweet enviado a las", ahora)
 
 if __name__ == "__main__":
     create_tweet("TEST", api_key, api_secret, access_token, access_secret)
