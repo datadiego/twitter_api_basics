4b146b8d77a28576c6334aab0b1fc5072fdcb3d2
Juan Diego <juandiegomariscal@gmail.com>
2022-07-18 23:07:15 +0200

    update: 🚀refactorizacion para obtener archivos dentro de la funcion

--- random_media_tweet.py
+++ random_media_tweet.py
@@ -15,15 +15,14 @@ def upload_random_media(api, img_path, text=""):
     img_path: Ruta a la imagen, video o sonido que queremos twittear
     text: (Opcional) Mensaje a twittear
     '''
-    media = api.media_upload(img_path)
+    files = os.listdir(img_path)
+    target_img = choice(files)
+    img = img_path+target_img
+    media = api.media_upload(img)
     api.update_status(text, media_ids=[media.media_id_string])
     print("Tweet enviado")      
 
 if __name__ == "__main__":
-    img_path = "imgs/"
-    files = os.listdir(img_path)
-    target_img = choice(files)
-    img_path += target_img
-    print(img_path)
+    img_path = "imgs/"    
     api = autentificar(api_key, api_secret, access_token, access_secret)
     upload_random_media(api, img_path)
